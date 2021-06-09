f219b910becfc6d61fe068fa0295b06963abfb6a
MohitKhandelwal06 <khandelwalmohit006@gmail.com>
2021-06-10 00:27:53 +0530

    modification 4

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get("/api",(req,res) =>
 
 app.get("/",(req,res)=>
 {
-res.sendFile((__dirname + "/ParkingSystemHome.html");
+res.sendFile((__dirname + "/ParkingSystemHome.html"));
 });
 app.get("/ParkingSystemHome",(req,res)=>
 {
